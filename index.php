423c7b9a9ac19c69b0bb54dcdb61bc7b1e1d4d01
Drichdev <rich10dev@gmail.com>
2023-12-12 09:38:12 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Inscription</title>
+</head>
+
+<body>
+    <div class="content">
+        <div class="cont-head">
+            <h1>Inscription</h1>
+            <img src="assets/icone-inscri.svg" alt="">
+        </div>
+        <div class="cont-body">
+            <form action="" method="post">
+                <label for="name">Nom</label>
+                <input id="name" type="text"><br>
+
+                <label for="surname">Prenom</label>
+                <input id="surname" type="text">
+
+                <label for="pass">Mot de passe</label>
+                <input id="pass" type="password">
+
+                <label for="confirm">Confirmer le mot de passe</label>
+                <input id="confirm" type="password">
+
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
