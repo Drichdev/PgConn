ed40a25cabf875cb34926907a859e83bea4eea21
Drichdev <rich10dev@gmail.com>
2023-12-19 12:34:06 +0000

    initial comiit

--- index.php
+++ index.php
@@ -9,28 +9,38 @@
 </head>
 
 <body>
-    <div class="content">
-        <div class="cont-head">
-            <h1>Inscription</h1>
-            <img src="assets/icone-inscri.svg" alt="">
+    <div class="all">
+
+        <div class="content">
+            <div class="cont-head">
+                <img src="assets/icone-inscri.svg" alt="">
+                <h1>Inscription</h1>
+            </div>
+            HATt OT LIKE
+            N <div class="cont-body">
+                <form action="" method="post">
+                    <label for="name">Nom :
+                        <input id="name" type="text">
+                    </label>
+
+                    <label for="surname">Prenom :
+                        <input id="surname" type="text">
+                    </label>
+
+                    <label for="pass">Mot de passe :
+                        <input id="pass" type="password">
+                    </label>
+
+                    <label for="confirm">Confirmer le mot de passe :
+                        <input id="confirm" type="password">
+                    </label>
+
+                </form>
+            </div>
         </div>
-        <div class="cont-body">
-            <form action="" method="post">
-                <label for="name">Nom</label>
-                <input id="name" type="text"><br>
 
-                <label for="surname">Prenom</label>
-                <input id="surname" type="text">
-
-                <label for="pass">Mot de passe</label>
-                <input id="pass" type="password">
-
-                <label for="confirm">Confirmer le mot de passe</label>
-                <input id="confirm" type="password">
-
-            </form>
-        </div>
     </div>
+
 </body>
 
 </html>
